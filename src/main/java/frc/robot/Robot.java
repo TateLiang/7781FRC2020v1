92b9b203b0da2b5961b589805b6948161f298812
Tate Liang <42977151+TateLiang@users.noreply.github.com>
2019-10-30 22:44:01 -0700

    remove pull

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.subsystems.DriveTrain;
 
-//PULL TEST
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
